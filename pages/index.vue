64d9d4610c9f54408526c038781005e84ccc2325
nakagawa <yusuke.1202.n@gmail.com>
2021-11-22 18:43:47 +0900

    [add]トップページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,230 @@
 <template>
-<div>
-    <shared-header></shared-header>
-    <h1>テストです</h1>
-    <img src="/img/skill_icon/angular.svg">
-    <router-link to="/works/ehon">絵本ページ</router-link>
+<div class="wrapper">
+      <!-- header -->
+      <shared-header></shared-header>
+      <!-- /header -->
+
+      <main class="content">
+        <!-- mv -->
+        <div class="mv">
+          <div class="mv-container">
+            <p class="mv-title">Yusuke Nakagawa's Portfolio</p>
+            <p class="mv-subtitle">Web Engineer</p>
+          </div>
+        </div>
+        <!-- /mv -->
+
+        <!-- about -->
+        <section class="about section" id="about">
+          <transition>
+          <div v-show="visible_about">
+          <div class="container">
+            <h2 class="title">About</h2>
+            <div class="profile">
+              <p class="profile-img">
+                <img src="@/assets/img/profile.jpg" alt="" />
+              </p>
+              <div class="profile-body">
+                <p>はじめまして！九州大学工学部土木工学科3年の中川雄介と申します。</p>
+                <p>
+                  大学1年の頃からプログラミングを始めて、これまでwebアプリやbotの作成、定型的な作業の自動化を行ってきました。<br />
+                  将来はITの道に進みたいと考えており、自分のアイデアを形にできるエンジニアになれるように日々努力を積み重ねています！
+                </p>
+              </div>
+            </div>
+          </div>
+          </div>
+          </transition>
+        </section>
+        <!-- /about -->
+
+                <!-- works -->
+        <section class="works section" id="works">
+            <transition>
+          <div v-show="visible_works">
+          <div class="container">
+            <h2 class="title">Works</h2>
+            <h3>Web Application</h3>
+              <div class="works-list">
+                <a class="works-item" v-bind:href="work.link" v-for="work in web_works" v-bind:key="work.name">
+                  <div class="works-img">
+                    <img v-bind:src="work.imgUrl" alt="" />
+                  </div>
+                  <p class="works-name">{{ work.name }}</p>
+                </a>
+              </div>
+
+              <!--
+              <div class="works-list" v-if="isShowed">
+                <a class="works-item" href="works/news.html">
+                  <div class="works-img">
+                    <img src="img/works/news/news.png" alt="" />
+                  </div>
+                  <p class="works-name">コロナウイルスに関するニュースをまとめたサイト</p>
+                </a>
+              </div>
+              <div class="show-works">
+                <button v-on:click="showWorks">{{ showWorksText }}</button>
+              </div>   
+              -->
+             <h3>LINE Bot</h3>
+             <div class="works-list">
+              <router-link class="works-item" v-bind:to="work.link" v-for="work in bot_works" v-bind:key="work.name">
+                <div class="works-img">
+                  <img v-bind:src="work.imgUrl" alt="" />
+                </div>
+                <p class="works-name">{{ work.name }}</p>
+              </router-link>
+             </div>
+          </div>   
+          </div>         
+            </transition>
+        </section>
+        <!-- /works -->
+
+        <!-- skill -->
+        <section class="skill section" id="skill">
+          <transition>
+          <div v-show="visible_skills">
+          <div class="container">
+            <h2 class="title">Skill</h2>
+            <div class="skill-list">
+              <div class="skill-item" v-for="skill in skills"  v-bind:key="skill.name">
+                <p class="skill-img">
+                  <img v-bind:src="skill.imgUrl" alt="" />
+                </p>
+                <div class="skill-body">
+                  <h3 class="skill-name">{{ skill.name }}</h3>
+                  <p class="skill-text">
+                    {{ skill.text }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          </transition>
+        </section>
+        <!-- /skill -->
+
+        <!-- contact -->
+        <section class="contact section" id="contact">
+          <transition>
+          <div v-show="visible_contact">
+          <div class="container">
+            <h2 class="title">Contact</h2>
+            <p class="lead">
+              お問い合わせは、<br class="sp-only"/>メールにてお願いいたします。
+            </p>
+            <div class="contact-list">
+              <a class="contact-item" href="https://twitter.com/uke_9912" target="_blank" rel="noopener">
+                <img src="@/assets/img/twitter.svg" alt="twitterのアイコン" />
+              </a>
+              <a class="contact-item" href="https://github.com/yusuke9912" target="_blank" rel="noopener">
+                <img src="@/assets/img/github.svg" alt="githubのアイコン"/>
+              </a>
+              <p class="mail-adress">nakagawa.1202.n@gmail.com</p>
+            </div>
+          </div>
+          </div>
+          </transition>
+        </section>
+        <!-- /contact -->
+      </main>
+
+      <!-- footer -->
+      <shared-footer></shared-footer>
+      <!-- /footer -->
     </div>
 </template>
 
+
 <script>
 import sharedHeader from "../components/shared/Header.vue"
+import sharedFooter from "../components/shared/Footer.vue"
+
 export default {
-    components:{
-        sharedHeader,
+  name: 'home',
+  data: function() {
+    return {
+      visible_about: true,
+      visible_works: true,
+      visible_skills: true,
+      visible_contact: true,
+    web_works:[
+      {link:"/works/ehon",imgUrl:"img/works/ehon/ehon.png",name:"保育用絵本アプリ"},
+      {link:"/works/chat",imgUrl:"img/works/chat/chat.png",name:"チャットアプリ"},
+      {link:"/works/typing",imgUrl:"img/works/typing/typing.png",name:"タイピングゲーム"},
+      {link:"/works/question",imgUrl:"img/works/questions/questions.png",name:"情報処理技術者試験の過去問演習アプリ"},
+      {link:"/works/todo",imgUrl:"img/works/todo/todo.png",name:"TODOリスト管理アプリ"},
+      {link:"/works/news",imgUrl:"img/works/news/news.png",name:"コロナウイルスに関するニュースをまとめたサイト"},
+    ],
+    bot_works:[
+      {link:"/works/board",imgUrl:"img/works/board_bot/board_bot.png",name:"土木工学科掲示板の更新を通知するLINEbot"},
+      {link:"/works/corona",imgUrl:"img/works/corona_bot/corona_bot.png",name:"福岡県のコロナウイルス感染者数を通知するLINEbot"},
+      {link:"/works/dogcat",imgUrl:"img/works/dog-cat_ai/dog-cat_ai.png",name:"犬と猫を判別するLINEbot"},
+    ],
+    skills:[
+      {imgUrl:"/img/skill_icon/html5.svg",name:"HTML5",text:"Webサイトの作成に使用しています。metaタグの記入や、ファビコンの設定も可能です。"},
+      {imgUrl:"/img/skill_icon/css3.svg",name:"CSS3",text:"Webサイトの作成に使用しています。レスポンシブデザインへの対応も可能です。"},
+      {imgUrl:"/img/skill_icon/javascript.svg",name:"JavaScript",text:"スライダーやハンバーガーメニューなどWebサイトの動的な処理の実装に使用しています。ライブラリとしてはjQuery,フレームワークとしてはAngularの使用経験があります。 Ajaxを用いた非同期通信の実装経験もあります。"},
+      {imgUrl:"/img/skill_icon/php.svg",name:"PHP",text:"Webアプリのバックエンドの実装に使用しています。PHPを使って過去問演習アプリやコロナに関するニュースをスクレイピングするアプリを作ったことがあります。"},
+      {imgUrl:"/img/skill_icon/ruby.svg",name:"Ruby",text:"Webアプリのバックエンドの実装に使用しています。フレームワークであるRuby on Railsを使用して、タイピングゲームを作ったことがあります。"},
+      {imgUrl:"/img/skill_icon/python.svg",name:"Python",text:"主にWebスクレイピングに使用しています。BeautifulSoupやSeleniumといったライブラリを使用して、twitterや情報処理技術者試験の過去問をスクレイピングしたことがあります。"},
+      {imgUrl:"/img/skill_icon/wordpress.svg",name:"WordPress",text:"Webサイトの作成に使用しています。インターンにおいて社内HPをWordPressを使って作成・修正しました。プラグインの導入、バックアップ、環境の移行の経験があります。"},
+      {imgUrl:"/img/skill_icon/firebase.svg",name:"Firebase",text:"Webアプリのバックエンドの実装に使用しています。Firebaseを用いたwebサイトの公開、ユーザー認証の実装、メール送信機能、DBの導入の経験があります。リアルタイムチャットアプリを作成する際に使用しました。"},
+      {imgUrl:"/img/skill_icon/aws.svg",name:"AWS",text:"Webアプリのインフラの実装に使用しています。AWSを用いて、Webサーバーを立ち上げサイトを公開したことがあります。その際は独自ドメインを取得し、プロキシサーバの導入やSSL化なども行いました。"},
+    ]
     }
+  },
+  methods:{
+    
+    handleScroll() {
+      let top;
+      if (!this.visible_about) {
+        top = document.getElementById("about").getBoundingClientRect().top;
+        this.visible_about = top < window.innerHeight -100;
+      }
+      if (!this.visible_works) {
+        top = document.getElementById("works").getBoundingClientRect().top;
+        this.visible_works = top < window.innerHeight -100;
+      }
+      if (!this.visible_skills) {
+        top = document.getElementById("skill").getBoundingClientRect().top;
+        this.visible_skills = top < window.innerHeight -100;
+      }
+      if (!this.visible_contact) {
+        top = document.getElementById("contact").getBoundingClientRect().top;
+        this.visible_contact = top < window.innerHeight -100;
+      }
+    }
+  },
+  components: {
+    sharedHeader,
+    sharedFooter
+  }
 }
 </script>
+
+
+<style>
+/*フェード処理ここから */
+.v-enter {
+  opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s;
+}
+
+.v-enter-to {
+  opacity: 1;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transition: opacity 1s;
+}
+/*フェード処理ここまで */
+</style>
