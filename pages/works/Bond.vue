c929a743e7cd80a6fd9c4073e4cd770cf2af2790
test <yusuke.1202.n@gmail.com>
2022-05-31 17:24:59 +0900

    [update]bond紹介ページの修正

--- pages/works/Bond.vue
+++ pages/works/Bond.vue
@@ -2,7 +2,7 @@
     <div class="wrapper">
 
       <!-- header -->
-      <shared-header></shared-header>
+      <!-- <shared-header></shared-header> -->
       <!-- /header -->
 
 <main class="content">
@@ -14,10 +14,11 @@
             <p class="article-image"> 
               <carousel :per-page="1">
                 <slide><img src="@/assets/img/works/bond/bond.png" alt="bondのスクリーンショット"></slide>
+                <slide><img src="@/assets/img/works/bond/bond2.png" alt="bondのスクリーンショット"></slide>
               </carousel>
             </p>
             <p>
-              Twitterで知り合った<a href="https://www.kyusan-u.ac.jp/news/social_business_plan_contest-6th/">中丸与志哉さん</a>率いるチーム「想」において、アプリの開発を行いました。<br>このアプリを通して大切な人へのメッセージを送りあってもらい、リアルな世界でのコミュニケーションを変えていきたいと思っています。
+              Twitterで知り合った<a href="https://www.kyusan-u.ac.jp/news/social_business_plan_contest-6th/">中丸与志哉さん率いるチーム「想」</a>において、大切な人にメッセージ動画を送りあうアプリの開発を行いました。<br>このアプリを通して大切な人へのメッセージを送りあってもらい、リアルな世界でのコミュニケーションを変えていきたいと思っています。
             </p>
             <h3>使用技術/ツール</h3>
             <ul class="skill-img">
@@ -25,10 +26,14 @@
               <li><img src="@/assets/img/skill_icon/firebase.svg" alt="Firebaseのアイコン"><span class="tooltip">Firebase</span></li>
             </ul>
             <h3>担当</h3>
-            <p>コーディング</p>
+            <p>デザイン・コーディング</p>
+            <h3>デザインについて</h3>
+            <p>
+              チーム内で話し合いながらアプリのフレームワークを作成しました。シンプルで使いやすいデザインを心掛けました。またApp Storeに載せるスクリーンショットやアイコンもチーム内でお互いに案を出し合い、作成しました。
+            </p>
             <h3>コーディングについて</h3>
             <p>
-              ユーザー認証やデータの保存はFirebaseを使用しました。またYoutubeの動画をアプリ内で再生できるようにYoutubeKitというライブラリを使用しました。
+              ユーザーの認証や動画データの保存はFirebaseを使用しました。またYoutubeの動画をアプリ内で再生できるようにYoutubeKitというライブラリを使用しました。初めてのネイティブアプリ開発ということもあり、試行錯誤しながら少しずつ開発を進めていきました。
             </p>
           </div>
           <div class="connection-link">
