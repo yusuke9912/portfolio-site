41641c90e59666dc2da454aaff740bb5601a34c7
test <yusuke.1202.n@gmail.com>
2022-06-06 00:47:57 +0900

    [update]contactの文言修正

--- pages/index.vue
+++ pages/index.vue
@@ -108,7 +108,7 @@
         <div class="container" :class="{fadeIn_up: visible_contact }">
           <h2 class="title">Contact</h2>
           <p class="lead">
-            お問い合わせは、<br class="sp-only" />下記SNSまたはメールからお願いいたします。
+            お問い合わせは、<br class="sp-only" />SNSまたはメールからお願いいたします。
           </p>
           <div class="contact-list">
             <a
